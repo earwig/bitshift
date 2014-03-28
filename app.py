f24d2a6be20eaa23e36a40224d7e0659f67644e0
Severyn Kozak <severyn.kozak@gmail.com>
2014-03-27 21:32:55 -0400

    Add assets/config module, SASS files, templates.
    Add:
    	bitshift/assets.py
    		-add module that contains functions to be called from inside the
    		templates/ Jinja HTML files -- currently contains tag(), which generates
    		an HTML asset tag based on a filename.
    
    	bitshift/config.py
    		-add Flask configuration module.
    
    	static/(sass/main.sass, css/main.css)
    		-create isolated directory for SASS files; compiled CSS files will be
    		stored in static/css.
    
    	static/css/_mixins.sass
    		-add SASS partial to contain mixins (globally relevant to the project's
    		styling).
    
    	templates/layout.html
    		-add various metadata.

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+"""
+Module to contain all the project's Flask server plumbing.
+"""
+
 from flask import Flask
-from flask import render_template
+from flask import render_template, session
+
+from bitshift import *
 
 app = Flask(__name__)
+app.config.from_object("bitshift.config")
 
-env = app.jinja_env
-app.jinja_env.line_statement_prefix = "="
+app_env = app.jinja_env
+app_env.line_statement_prefix = "="
+app_env.globals.update(assets = assets)
 
 @app.route("/")
 def index():
---
+++ bitshift/config.py
@@ -0,0 +1,6 @@
+"""
+Module to contain definitions of all Flask variables required by the app module.
+"""
+
+DEBUG = True
+SECRET_KEY = "\x89\x87\x9a9\xab{\xda\xfe.28\xb4\x18\x01\x95]]\xd2\xeaen\xe0Ot"
