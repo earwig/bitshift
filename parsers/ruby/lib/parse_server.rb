be7c871cc96e9b328afe8a754c258e55391d5e4a
Benjamin Attal <attal.benjamin@gmail.com>
2014-05-05 17:21:45 -0400

    Add Rakefile task for running ruby parse server.

--- parsers/ruby/lib/parse_server.rb
+++ parsers/ruby/lib/parse_server.rb
@@ -12,22 +12,25 @@ def pack_int(i)
     return bytes.pack('cccc')
 end
 
-server = TCPServer.new 5003
 
-loop do
-    # Start a new thread for each client accepted
-    Thread.start(server.accept) do |client|
-        begin
-            # Get the amount of data to be read
-            size = (client.readline).to_i
-            p = Bitshift::Parser.new client.read(size)
-            # Get the parsed result
-            symbols = p.parse
-            client.puts pack_int(symbols.length)
-            client.puts symbols
-        ensure
-            # Close the socket
-            client.close
+def start_server
+    server = TCPServer.new 5003
+
+    loop do
+        # Start a new thread for each client accepted
+        Thread.start(server.accept) do |client|
+            begin
+                # Get the amount of data to be read
+                size = (client.readline).to_i
+                p = Bitshift::Parser.new client.read(size)
+                # Get the parsed result
+                symbols = p.parse
+                client.puts pack_int(symbols.length)
+                client.puts symbols
+            ensure
+                # Close the socket
+                client.close
+            end
         end
     end
 end
