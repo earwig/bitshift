6edb142b4a7e9f854f9c0c0f32dcf7a2572384c5
Benjamin Attal <attal.benjamin@gmail.com>
2014-06-01 00:18:02 -0400

    Can specify port number to run java server on.

--- parsers/java/src/main/java/com/bitshift/parsing/Parse.java
+++ parsers/java/src/main/java/com/bitshift/parsing/Parse.java
@@ -1,24 +1,12 @@
 package com.bitshift.parsing;
 
-import java.io.IOException;
-
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import com.bitshift.parsing.parsers.JavaParser;
+import com.bitshift.parsing.utils.ParseServer;
 
 public class Parse {
 
     public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(5033);
-
-            while(true) {
-                Socket clientSocket = server.accept();
-                new Thread(new JavaParser(clientSocket)).start();
-            }
-        } catch (IOException ex) {
-        }
+        ParseServer server = new ParseServer(Integer.parseInt(args[0]));
+        new Thread(server).start();
     }
 
 }
