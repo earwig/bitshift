73dee778c58e30bdd5796257ceb05960fa287187
Benjamin Attal <attal.benjamin@gmail.com>
2014-06-01 01:29:01 -0400

    Can specify which port to run ruby server on.

--- parsers/java/src/main/java/com/bitshift/parsing/Parse.java
+++ parsers/java/src/main/java/com/bitshift/parsing/Parse.java
@@ -6,6 +6,7 @@ public class Parse {
 
     public static void main(String[] args) {
         ParseServer server = new ParseServer(Integer.parseInt(args[0]));
+        System.out.println("Java Server listening on port " + args[0]);
         new Thread(server).start();
     }
 
