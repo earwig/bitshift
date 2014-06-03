024f0c7afb74e40c43fd9e7ab9630efd56a9db85
Ben Kurtovic <ben.kurtovic@gmail.com>
2014-06-03 00:05:29 -0400

    Rawr.

--- bitshift/languages.py
+++ bitshift/languages.py
@@ -2,4 +2,4 @@ import json
 from os import path
 
 with open(path.join(path.dirname(__file__), "languages.json")) as lang_json:
-    LANGS = [str(lang) for lang in json.load(lang_json)["languages"]]
+    LANGS = [lang.encode("utf8") for lang in json.load(lang_json)["languages"]]
