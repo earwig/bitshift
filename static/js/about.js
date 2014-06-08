63dfa8277fd807920dd516cc8848bd4d2a133bc1
Severyn Kozak <severyn.kozak@gmail.com>
2014-06-08 12:59:25 -0400

    Clean up style rules, improve positioning.
    Add:
            static/sass/about.sass
                    -Clean up existing style rules.
                    -Add rules for the about sections' text.
    
            templates/about.html
                    -Integrate with `templates/layout.html`, the project's global
                    HTML template.

--- static/js/about.js
+++ static/js/about.js
@@ -6,9 +6,6 @@ function parallax(){
     $(".bg").each(function(){
         $(this).css("top", parseFloat($(this).css("top")) - delta * 1.35 + "px");
     });
-    $(".bg#img-1").each(function(){
-        console.log($(this).css("top"));
-    });
     lastVertPos = currVertPos;
 }
 
