2cc5be8737f16ddb328a156234a9e1b88f7b6a0c
Severyn Kozak <severyn.kozak@gmail.com>
2014-06-08 13:41:19 -0400

    Update footer, minor changes to /about styling.
    Add:
            static/sass/main.sass
                    -Rework the footer, which is now fixed at the bottom of the
                    screen as opposed to floored to the bottom of the page.
                    Positioning at the bottom of the page doesn't make any sense
                    with a technically infinite results page.

--- static/js/about.js
+++ static/js/about.js
@@ -4,7 +4,7 @@ function parallax(){
     var currVertPos = $(window).scrollTop();
     var delta = currVertPos - lastVertPos;
     $(".bg").each(function(){
-        $(this).css("top", parseFloat($(this).css("top")) - delta * 1.35 + "px");
+        $(this).css("top", parseFloat($(this).css("top")) - delta * 1.8 + "px");
     });
     lastVertPos = currVertPos;
 }
