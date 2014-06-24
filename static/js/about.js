54cbd29e4208d062ab621c869ad1447c9da6786b
Severyn Kozak <severyn.kozak@gmail.com>
2014-06-24 14:09:45 -0400

    Reformat about.html, embed vimeo demo video.
    Add:
            templates/about.html. static/(sass/about.sass, js/about.js)
                    -Embed our Vimeo video, and add the Javascript necessary to make
                    it fluid-width.
    
    Ref:
            templates/about.html
                    -The file's indented formatting was completely mangled in a
                    previous commit; reformat.

--- static/js/about.js
+++ static/js/about.js
@@ -4,6 +4,10 @@
 
 var lastVertPos = $(window).scrollTop();
 
+/*
+ * Scroll `div#img-[1-4]` at a greater speed than the text, producing a
+ * parallax effect.
+*/
 $(window).scroll(function(e){
     var currVertPos = $(window).scrollTop();
     var delta = currVertPos - lastVertPos;
@@ -13,3 +17,30 @@ $(window).scroll(function(e){
     });
     lastVertPos = currVertPos;
 });
+
+/*
+ * Make the dimensions of the vimeo video fluid.
+*/
+(function adjustVimeoDimensions(){
+    var iframe = $("iframe#vimeo")[0];
+    var videoRatio = (iframe.height / iframe.width) * 100;
+
+    iframe.style.position = "absolute";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.width = "100%";
+    iframe.height = "100%";
+
+    iframe.style.height = "";
+    iframe.style.width = "";
+
+    var wrap = document.createElement("div");
+    wrap.className = "fluid-vids";
+    wrap.style.width = "100%";
+    wrap.style.position = "relative";
+    wrap.style.paddingTop = videoRatio + "%";
+
+    var iframeParent = iframe.parentNode;
+    iframeParent.insertBefore(wrap, iframe);
+    wrap.appendChild(iframe);
+}());
